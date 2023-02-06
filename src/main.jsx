1c20b00ddfadf5d0766096a6a5736ca452a23548
Jansenck <caik11@hotmail.com>
2023-02-05 21:51:31 -0300

    refactoring app render

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { render } from "react-dom";
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const container = document.getElementById("root");
+
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  container
 );
